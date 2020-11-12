1ea93a4781830fb172d2611ad5c6fc47dbc401dc
syahmisenpai97 <74369439+syahmisenpai97@users.noreply.github.com>
2020-11-13 00:11:29 +0800

    Add files via upload

---
+++ 3 to 1 mux/alu.v
@@ -0,0 +1,19 @@
+module alu(a, b, f, r);
+input [31:0] a,b;
+input [2:0] f;
+output [31:0] r;
+
+
+
+
+wire [31:0] addmux_out, submux_out;
+wire [31:0] add_out, sub_out, mul_out;
+
+mux32two adder_mux(b, 32'd1, f[0], addmux_out);
+mux32two sub_mux(b, 32'd1, f[0], submux_out);
+add32 our_adder(a,addmux_out,add_out);
+sub32 our_subtracter(a,submux_out,sub_out);
+mul16 our_multiplier(a[15:0], b[15:0], mul_out);
+mux32three output_mux(add_out,sub_out,mul_out,f[2:1],r);
+
+endmodule
